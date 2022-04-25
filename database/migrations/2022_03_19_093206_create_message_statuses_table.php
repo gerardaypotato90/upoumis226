69b7f14cc577afd0779a108f12bcf935f7bc931e
gerardaypotato90 <gerard.alainp@gmail.com>
2022-04-25 21:14:01 +0800

    Gerard
    new files

---
+++ database/migrations/2022_03_19_093206_create_message_statuses_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('message_status', function (Blueprint $table) {
+            $table->unsignedSmallInteger('id')->primary();
+            $table->string('status_name',42)->unique();
+            $table->string('status_desc',100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('message_status');
+    }
+};
